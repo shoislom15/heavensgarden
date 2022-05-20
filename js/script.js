9abdb0701d9155e9ede938fb61df0668320457f6
August <shoislomshobaxromov@gmail.com>
2022-05-20 20:29:17 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// $(document).ready(function () {
+//     $('.room__cards').slick({
+//         slidesToShow: 3,
+//         slidesToScroll: 1,
+//         autoplay: true,
+//         autoplaySpeed: 2000,
+//     });
+// })
