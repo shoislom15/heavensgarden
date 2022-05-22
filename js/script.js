25fcd0901875bf9e04209bd4d3186449c79a8603
August <shoislomshobaxromov@gmail.com>
2022-05-22 18:55:38 +0500

    responsive navbar corrected

--- js/script.js
+++ js/script.js
@@ -1,14 +1,33 @@
-const headerClose = document.getElementById("closeBtn");
-const headerNavbar = document.getElementById("hidenNav");
-const burger = document.getElementById("burger");
 const body = document.body;
 
-burger.onclick = () => {
-  headerNavbar.classList.add("open");
-  body.style.overflowY = "hidden";
+const close = (element, className) => {
+  body.style.overflowY = "visible";
+  element.classList.remove(className);
 };
 
-headerClose.onclick = () => {
-  body.style.overflowY = "visible";
-  headerNavbar.classList.remove("open");
+const open = (element, className) => {
+  body.style.overflowY = "hidden";
+  element.classList.add(className);
 };
+
+const headerToggler = () => {
+  const headerClose = document.getElementById("closeBtn");
+  const headerNavbar = document.getElementById("hidenNav");
+  const burger = document.getElementById("burger");
+  const ul = document.querySelector("#hidenNav ul");
+
+  burger.onclick = () => {
+    open(headerNavbar, "open");
+  };
+
+  headerClose.onclick = () => close(headerNavbar, "open");
+
+  ul.addEventListener("click", (e) => {
+    // console.log(e.target.tagName);
+    if (e.target.tagName == "A") {
+      close(headerNavbar, "open");
+    }
+  });
+};
+
+headerToggler();
